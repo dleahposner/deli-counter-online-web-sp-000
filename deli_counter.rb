34abaafa30755ebb8ee844ab25f697620d8c0606
Devorah Posner <dleahposner@gmail.com>
2021-02-01 21:49:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 require "pry"
+=======
+>>>>>>> cca20f04cb068c5cac6b5781732a240c71625e84
 katz_deli = []
 
 def line(katz_deli)
@@ -15,17 +18,28 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push "#{name}"
+<<<<<<< HEAD
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
  end
  
 
+=======
+  katz_deli.each_with_index do |name, index|
+  puts "Welcome, #{name}. You are number #{index+1} in line."
+ end
+ 
+end
+>>>>>>> cca20f04cb068c5cac6b5781732a240c71625e84
 
 def now_serving(katz_deli)
   if katz_deli == []
   puts "There is nobody waiting to be served!"
 else
+<<<<<<< HEAD
  puts "Currently serving #{katz_deli.first}."
  katz_deli.shift
+=======
+>>>>>>> cca20f04cb068c5cac6b5781732a240c71625e84
 end
 end
 
